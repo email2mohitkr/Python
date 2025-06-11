f8c044f8e0bf49ddc887214f3f03596ab659ed3f
email2mohitkr <111981131+email2mohitkr@users.noreply.github.com>
2025-06-11 19:37:55 +0530

    Add files via upload

---
+++ Task1.py
@@ -0,0 +1,23 @@
+
+'''
+Write a Python program that does the following:
+1.  Takes two numbers as input from the user.
+2.  Performs the basic mathematical operations on these two numbers:
+o	Addition
+o	Subtraction
+o	Multiplication
+o	Division
+3.  Displays the results of each operation on the screen.
+'''
+
+a=input('Enter The First Number : ' )
+a=float(a)
+b=input('Enter The Secound number : ')
+b=float(b)
+
+
+print('Addition : ', a+b)
+print('Subtraction : ', a-b)
+print('Multiplication : ', a*b)
+print('Division : ', a/b)
+
---
+++ Task2.py
@@ -0,0 +1,14 @@
+'''
+Write a Python program that:
+1.  Takes a user's first name and last name as input.
+2.  Concatenates the first name and last name into a full name.
+3.  Prints a personalized greeting message using the full name.
+'''
+
+
+
+
+f=input('Enter your first name : ' )
+l=input('Enter your last name : ')
+
+print('Hello,',f,l+'! Welcome to the python program.')
